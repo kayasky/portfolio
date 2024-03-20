7850f51b0e85ec598fd72a72db14102351f81d08
Kaya Jhuti <itskayaj@gmail.com>
2024-03-20 09:24:55 -0400

    Add buttons to intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -3,10 +3,13 @@
 import portrait from "@/public/portrait.jpeg";
 import { motion } from "framer-motion";
 import Image from "next/image";
+import Link from "next/link";
+import { BsArrowRight, BsGithub, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -41,7 +44,11 @@ export default function Intro() {
           </motion.div>
         </div>
       </div>
-      <p>
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+      >
         <span className="font-bold">Hi, I'm Kaya.</span> I'm a{" "}
         <span className="font-bold">full-stack developer</span> with{" "}
         <span className="font-bold">over 10 years</span> of experience. I enjoy
@@ -50,7 +57,34 @@ export default function Intro() {
           Typescript, Angular & React with Next.js
         </span>
         .
-      </p>
+      </motion.p>
+
+      <div className="flex gap-2">
+        <Link
+          href="#contact"
+          className="bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full"
+        >
+          Contact me here <BsArrowRight />
+        </Link>
+        <a
+          href=""
+          className="bg-white px-7 py-3 flex items-center gap-2 rounded-full"
+        >
+          Download Resume <HiDownload />
+        </a>
+        <a
+          href=""
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href=""
+          className="bg-white text-gray-700 p-4 flex items-center gap-2 rounded-full text-[1.2rem]"
+        >
+          <BsGithub />
+        </a>
+      </div>
     </section>
   );
 }
