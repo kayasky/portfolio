d2a76798f3e04c7ed6d866e091c315c26a0c651c
Kaya Jhuti <itskayaj@gmail.com>
2024-03-23 22:20:19 -0400

    Comment out divider for now

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ import SectionDivider from "@/components/section-divider";
 export default function Home() {
   return <main className="flex flex-col items-center px-4">
     <Intro />
-    <SectionDivider />
+    {/* <SectionDivider /> */}
   </main>;
 }
