6c19dce12dd59cf4bd0c38dee1b6fc3d9a879e45
Kaya Jhuti <itskayaj@gmail.com>
2024-03-26 22:11:56 -0400

    Start work on the projects section

---
+++ components/projects.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+
+export default function Projects() {
+  return (
+    <motion.section>
+      <SectionHeading>My Projects</SectionHeading>
+      <div></div>
+    </motion.section>
+  );
+}
