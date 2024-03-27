b1956a5d2e00fca026cf4442349c009a3051dac0
Kaya Jhuti <itskayaj@gmail.com>
2024-03-26 21:26:50 -0400

    Add animation

--- components/about.tsx
+++ components/about.tsx
@@ -5,7 +5,12 @@ import SectionHeading from "./section-heading";
 
 export default function About() {
   return (
-    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+    <motion.section
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40"
+    >
       <SectionHeading>About Me</SectionHeading>
       <p className="mb-3">
         With over 10 years of experience in web development, I am passionate
