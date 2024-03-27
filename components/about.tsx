aced52329681b1de02e29c55fbe56f665cc285ad
Kaya Jhuti <itskayaj@gmail.com>
2024-03-26 21:24:38 -0400

    Add section heading

--- components/about.tsx
+++ components/about.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+
 export default function About() {
   return (
-    <section>
-      <h2>About me</h2>
-      <p></p>
-      <p></p>
-    </section>
+    <motion.section className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40">
+      <SectionHeading>About Me</SectionHeading>
+      <p className="mb-3">
+        With over 10 years of experience in web development, I am passionate
+        about creating innovative and user-friendly products that meet the needs
+        of various industries, such as banking, wealth management, and telecom.
+      </p>
+    </motion.section>
   );
 }
