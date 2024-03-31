9903b9c48ffd91d8f2df9a24a5199b587ec95be6
Kaya Jhuti <itskayaj@gmail.com>
2024-03-31 17:38:15 -0400

    Add projects markup

--- components/projects.tsx
+++ components/projects.tsx
@@ -2,12 +2,22 @@
 
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
 
 export default function Projects() {
   return (
     <motion.section>
       <SectionHeading>My Projects</SectionHeading>
-      <div></div>
+      <div>
+        {
+          projectsData.map((project) => (
+            <div key={project.title}>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </div>
+          ))
+        }
+      </div>
     </motion.section>
   );
 }
