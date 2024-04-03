a71ffe5ca739537721150e602b83ca34de9b6046
Kaya Jhuti <itskayaj@gmail.com>
2024-04-02 21:39:18 -0400

    Start adding parojects

--- components/projects.tsx
+++ components/projects.tsx
@@ -3,21 +3,25 @@
 import { motion } from "framer-motion";
 import SectionHeading from "./section-heading";
 import { projectsData } from "@/lib/data";
+import React from "react";
 
 export default function Projects() {
   return (
     <motion.section>
       <SectionHeading>My Projects</SectionHeading>
       <div>
-        {
-          projectsData.map((project) => (
-            <div key={project.title}>
-              <h3>{project.title}</h3>
-              <p>{project.description}</p>
-            </div>
-          ))
-        }
+        {projectsData.map((project, index) => (
+          <React.Fragment key={index}>
+            <Project {...project} />
+          </React.Fragment>
+        ))}
       </div>
     </motion.section>
   );
+}
+
+type ProjectProps = (typeof projectsData)[number];
+
+function Project({ title, description, tags, imageUrl }: ProjectProps) {
+  return <div>{title}</div>;
 }
