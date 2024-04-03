bbce6c6d6a772641d9e4060c5cdc9475b856ae22
Kaya Jhuti <itskayaj@gmail.com>
2024-04-03 18:57:02 -0400

    Start work on adding an action to the contact form

--- components/contact.tsx
+++ components/contact.tsx
@@ -1,33 +1,48 @@
-import React from "react";
-import SectionHeading from "./section-heading";
+"use client";
+
+import { motion } from "framer-motion";
 import { FaPaperPlane } from "react-icons/fa";
+import SectionHeading from "./section-heading";
 
 export default function Contact() {
   return (
-    <section
+    <motion.section
       id="contact"
       className="mb-20 sm:mb-28 w-[min(100%,38rem)] text-center"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
     >
       <SectionHeading>Contact me</SectionHeading>
       <p className="text-grey-700">Please contact me using the form below :)</p>
-      <form className="mt-10 flex flex-col">
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          console.log(formData.get("senderEmail"));
+          console.log(formData.get("message"));
+        }}
+      >
         <input
           className="h-14 rounded-lg borderBlack p-4"
           type="email"
           placeholder="Your email"
           required
+          name="senderEmail"
+          maxLength={500}
         />
         <textarea
           className="h-52 my-3 rounded-lg borderBlack p-4"
           placeholder="Your message"
           required
+          name="message"
+          maxLength={500}
         ></textarea>
         <button
           type="submit"
           className="group flex items-center gap-2 justify-center h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all
           focus:scale-110 hover:scale-110 active:scale-105 hover:bg-gray-950"
         >
-          Send{" "}
+          Send
           <FaPaperPlane
             className="text-xs opacity-70 transition-all
           group-hover:translate-x-1
@@ -35,6 +50,6 @@ export default function Contact() {
           />
         </button>
       </form>
-    </section>
+    </motion.section>
   );
 }
